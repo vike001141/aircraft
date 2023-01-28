1350af2843cadbc9743f4a03fd7d5d4a0dfb4e6b
Frank Kopp <frank@familie-kopp.de>
2023-01-28 23:05:37 +0100

    Rebased to new repo structure

--- igniter.config.mjs
+++ igniter.config.mjs
@@ -190,13 +190,6 @@ export default new TaskOfTasks("all", [
                 "fbw-a380x/out/flybywire-aircraft-a380-842/SimObjects/AirPlanes/FlyByWire_A380_842/panel/terronnd.wasm",
                 "fbw-common/src/wasm/terronnd/out/terronnd.wasm",
             ]),
-            new ExecTask("flypad-backend",
-                "npm run build-a380x:flypad-backend",
-                [
-                    "fbw-a380x/src/wasm/flypad-backend",
-                    "fbw-common/src/wasm/fbw_common",
-                    "fbw-a380x/out/flybywire-aircraft-a380-842/SimObjects/AirPlanes/FlyByWire_A380_842/panel/flypad-backend.wasm"
-                ])
         ], true)
     ]),
 
