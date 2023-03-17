2aa2ffc6adbced9206e84e5f2afbe386fe2e3258
Frank Kopp <frank@familie-kopp.de>
2023-03-17 23:18:17 +0100

    Rename the build script tp prepare for it to be valid for all cpp wasm modules

--- igniter.config.mjs
+++ igniter.config.mjs
@@ -134,7 +134,7 @@ export default new TaskOfTasks("all", [
                 "fbw-common/src/wasm/terronnd/out/terronnd.wasm",
             ]),
             new ExecTask('extra-backend-a32nx',
-                "npm run build:extra-backend-cmake",
+                "npm run build:cpp-wasm-cmake",
                 [
                     'fbw-a32nx/src/wasm/extra-backend',
                     'fbw-common/src/wasm/extra-backend',
@@ -194,7 +194,7 @@ export default new TaskOfTasks("all", [
                 "fbw-common/src/wasm/terronnd/out/terronnd.wasm",
             ]),
             new ExecTask('extra-backend-a380x',
-                "npm run build:extra-backend-cmake",
+                "npm run build:cpp-wasm-cmake",
                 [
                     'fbw-a380x/src/wasm/extra-backend',
                     'fbw-common/src/wasm/extra-backend',
